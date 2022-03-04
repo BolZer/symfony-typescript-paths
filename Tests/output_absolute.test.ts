05e03abd1f7c2459078c1a84968f0973880c055c
bolZer <jan.noehles@easybill.de>
2022-03-04 21:01:29 +0100

    Add typescript tests

---
+++ Tests/output_absolute.test.ts
@@ -0,0 +1,17 @@
+import {path_user_route} from "./output_absolute";
+
+test('test path_user_route absolute route', () => {
+    const result1 = path_user_route().absolute({id: "exampleID", noteId: "exampleNoteID"})
+    expect(result1).toBe('https://app.development.org/user/exampleID/notes/exampleNoteID');
+
+    const result2 = path_user_route().absolute({id: "exampleID", noteId: "exampleNoteID"}, {count: "20", page: "3"})
+    expect(result2).toBe('https://app.development.org/user/exampleID/notes/exampleNoteID?count=20&page=3');
+});
+
+test('test specific generation of only absolute routes', () => {
+    const routeObject = path_user_route();
+
+    expect(routeObject).toHaveProperty("absolute");
+    expect(routeObject).not.toHaveProperty("relative");
+    expect(Object.keys(routeObject).length).toBe(1)
+});
